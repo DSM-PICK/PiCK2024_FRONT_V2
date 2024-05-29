dd62bf91328b5217a57407ccca802bea9510da2e
phyuna0525 <phyunaa0525@gmail.com>
2024-05-29 23:11:12 +0900

    add: main page

---
+++ src/pages/Main/index.tsx
@@ -0,0 +1,20 @@
+import Header from "components/header";
+import * as S from "./style";
+import mainLogo from "assets/svg/mainLogo.svg";
+import MainRouterButton from "components/mainRouterButton";
+
+const Main = () => {
+  return (
+    <>
+      <Header />
+      <S.MainContainer>
+        <img src={mainLogo} alt="" />
+        <S.ButtonBox>
+          <MainRouterButton />
+        </S.ButtonBox>
+      </S.MainContainer>
+    </>
+  );
+};
+
+export default Main;
---
+++ src/pages/Main/style.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const MainContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  gap: 120px;
+  margin-top: 80px;
+  user-select: none;
+`;
+
+export const ButtonBox = styled.div`
+  width: 890px;
+`;
