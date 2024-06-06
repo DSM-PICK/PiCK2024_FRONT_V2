57947b1210748ffa5ffea7f2900ce0ff7d671f43
heedda <heedda.kollim@gmail.com>
2024-06-06 12:56:55 +0900

    add: dropdown

---
+++ src/components/dropdown/index.tsx
@@ -0,0 +1,83 @@
+import { useEffect, useRef, useState } from "react";
+import * as S from "./style";
+import dropdownArrowImg from "assets/svg/dropdownImg.svg";
+
+interface DropdownProps {
+  type: "floor" | "grade" | "class";
+}
+
+const Dropdown = ({ type }: DropdownProps) => {
+  const [dropdownArray, setDropdownArray] = useState<string[]>([]);
+  const [dropdownImg, setDropdownImg] = useState(dropdownArrowImg);
+  const [dropdownTitle, setDropdownTitle] = useState<string>();
+  const [isActive, setIsActive] = useState(false);
+  const dropdownRef = useRef<HTMLDivElement | null>(null);
+
+  const floorArray = ["1층", "2층", "3층", "4층", "5층"];
+  const gradeArray = ["1학년", "2학년", "3학년"];
+  const classArray = ["1반", "2반", "3반", "4반"];
+
+  useEffect(() => {
+    switch (type) {
+      case "floor":
+        setDropdownArray(floorArray);
+        setDropdownTitle(floorArray[0]);
+        break;
+      case "grade":
+        setDropdownArray(gradeArray);
+        setDropdownTitle(gradeArray[0]);
+        break;
+      case "class":
+        setDropdownArray(classArray);
+        setDropdownTitle(classArray[0]);
+        break;
+    }
+  }, []);
+
+  useEffect(() => {
+    const clickOutside = (e: MouseEvent) => {
+      if (
+        isActive &&
+        dropdownRef.current &&
+        !dropdownRef.current.contains(e.target as Node)
+      ) {
+        setIsActive(false);
+      }
+    };
+
+    document.addEventListener("mousedown", clickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", clickOutside);
+    };
+  }, [isActive]);
+
+  const handleDropdown = () => {
+    setIsActive(!isActive);
+    setDropdownImg(dropdownArrowImg);
+  };
+
+  return (
+    <S.PositionDiv>
+      <S.DropdownTitle onClick={handleDropdown} ref={dropdownRef}>
+        {dropdownTitle}
+        <img src={dropdownImg} alt="" />
+      </S.DropdownTitle>
+      <S.DropdownClickContainer isActive={isActive}>
+        {dropdownArray.map((element, index) => (
+          <S.DropdownClick
+            key={index}
+            isActive={isActive}
+            onClick={() => {
+              setDropdownTitle(element);
+            }}
+          >
+            {element}
+          </S.DropdownClick>
+        ))}
+      </S.DropdownClickContainer>
+    </S.PositionDiv>
+  );
+};
+
+export default Dropdown;
