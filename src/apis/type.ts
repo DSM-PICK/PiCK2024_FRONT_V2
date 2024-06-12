83582b807c149f407f68671a79efae0e0a17b283
phyuna0525 <phyunaa0525@gmail.com>
2024-06-12 21:10:51 +0900

    add : notice

--- src/apis/type.ts
+++ src/apis/type.ts
@@ -16,3 +16,35 @@ export interface StateCountType {
   out: number;
   class_move: number;
 }
+
+export interface SimpleNoticeType {
+  id: string;
+  title: string;
+  create_at: string;
+  teacher: string;
+  grade: number[];
+}
+
+export interface MynameType {
+  name: string;
+  grade: number;
+  class_num: number;
+}
+
+export interface NoticeDetailType {
+  id: string;
+  title: string;
+  create_at: string;
+  teacher: string;
+  content: string;
+}
+export interface ChangeProps {
+  text: string;
+  name: string;
+}
+
+export interface UploadNoticeType {
+  title: string;
+  content: string;
+  grade: number[];
+}
---
+++ src/pages/notice/write/index.tsx
@@ -0,0 +1,104 @@
+import { Layout } from "components/layout";
+import { useNavigate } from "react-router-dom";
+import nextSvg from "assets/svg/next.svg";
+import Input from "components/input";
+import * as S from "../style";
+import { getFullToday } from "utils/date";
+import BottomButtonWrap from "components/Button/bottom";
+import { useState } from "react";
+import { ChangeProps } from "apis/type";
+import { UploadNotice } from "apis/notice";
+
+const NoticeWrite = () => {
+  const router = useNavigate();
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+
+  const { mutate: UploadNoticeMutate } = UploadNotice();
+
+  const handleTitleChange = ({ text, name }: ChangeProps) => {
+    setTitle(text);
+  };
+
+  const handleContentChange = (
+    event: React.ChangeEvent<HTMLTextAreaElement>
+  ) => {
+    setContent(event.target.value);
+  };
+
+  const name = localStorage.getItem("name");
+
+  const Upload = () => {
+    UploadNoticeMutate(
+      {
+        title: title,
+        content: content,
+        grade: [4],
+      },
+      {
+        onSuccess: () => {
+          //성공시 토스트
+        },
+        onError: () => {
+          //에러시 토스트
+        },
+      }
+    );
+  };
+
+  return (
+    <>
+      <Layout
+        now={
+          <>
+            <p
+              onClick={() => {
+                router("/notice");
+              }}
+            >
+              공지
+            </p>
+            <img src={nextSvg} alt="" /> <p>공지 작성하기</p>
+          </>
+        }
+        title="공지 작성하기"
+      >
+        <S.NoticeWriteTop>
+          <S.NoticeWrite>
+            <S.NoticeBedgeTitle>작성자</S.NoticeBedgeTitle>
+            <S.NoticeBedge>{name} 선생님</S.NoticeBedge>
+          </S.NoticeWrite>
+          <S.NoticeWrite>
+            <S.NoticeBedgeTitle>작성일</S.NoticeBedgeTitle>
+            <S.NoticeBedge>{getFullToday()}</S.NoticeBedge>
+          </S.NoticeWrite>
+        </S.NoticeWriteTop>
+        <Input
+          widthtype="long"
+          name="title"
+          onChange={handleTitleChange}
+          value={title}
+          label="*제목"
+          placeholder="공지 제목을 입력하세요"
+        />
+        <S.TextareaWrap>
+          <S.InputLabel>*내용</S.InputLabel>
+          <S.WriteTextarea
+            name="content"
+            onChange={handleContentChange}
+            placeholder="공지 내용을 입력하세요"
+            value={content}
+          />
+        </S.TextareaWrap>
+      </Layout>
+      <BottomButtonWrap
+        firstDisabled={false}
+        firstOnclick={Upload}
+        firstSize="standard"
+        firstType="main"
+      />
+    </>
+  );
+};
+
+export default NoticeWrite;
