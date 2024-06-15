104f89f5e89b90eff6d7aa5986854422abda0810
phyuna0525 <phyunaa0525@gmail.com>
2024-06-15 23:38:12 +0900

    add : 외출 수락

---
+++ src/pages/outAccept/style.ts
@@ -0,0 +1,10 @@
+import { styled } from "styled-components";
+import { theme } from "styles/theme";
+
+export const OutAcceptContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  row-gap: 40px;
+  column-gap: 60px;
+`;
