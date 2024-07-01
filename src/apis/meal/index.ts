def9d6d19b76d5dd546fbb587b4299a98f3b4243
phyuna0525 <phyunaa0525@gmail.com>
2024-07-01 17:15:17 +0900

    add : 주말급식 엑셀 출력

--- src/apis/meal/index.ts
+++ src/apis/meal/index.ts
@@ -1,4 +1,4 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useMutation } from "@tanstack/react-query";
 import { instance } from "@/apis";
 import { TodayMealsType } from "@/apis/type";
 import { getFullToday } from "@/utils/date";
@@ -15,4 +15,27 @@ export const TodayMeals = () => {
       return data;
     },
   });
+};
+
+export const DownLoad = () => {
+  const downloadExcel = async () => {
+    try {
+      const response = await instance.get("/weekend-meal/excel", {
+        responseType: "blob",
+      });
+
+      const url = window.URL.createObjectURL(new Blob([response.data]));
+      const link = document.createElement("a");
+      link.href = url;
+      link.setAttribute("download", "주말급식리스트.xlsx");
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    } catch (error) {
+      console.error("Error while downloading Excel file:", error);
+      alert("엑셀 다운로드에 실패하였습니다");
+    }
+  };
+
+  return { downloadExcel };
 };
