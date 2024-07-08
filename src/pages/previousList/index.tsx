7764af62ceed35ccf320b011ead1f794561af94a
phyuna0525 <phyunaa0525@gmail.com>
2024-07-08 11:49:34 +0900

    add : 이전외출기록 상세보기 및 api연동

--- src/pages/previousList/index.tsx
+++ src/pages/previousList/index.tsx
@@ -1,13 +1,34 @@
+import { AllList } from "@/apis/story";
 import SearchInput from "@/components/input/search";
 import { Layout } from "@/components/layout";
+import StoryList from "@/components/story/story";
+import { getStudentString, setStudentNum } from "@/utils/utils";
 import { useState } from "react";
+import { styled } from "styled-components";
 
 const PreviousList = () => {
   const [searchTerm, setSearchTerm] = useState("");
+  const { data: GetCount } = AllList();
 
-  const handleSearchChange = ({ text }: { text: string; name: string }) => {
+  const handleSearchChange = ({
+    text,
+    name,
+  }: {
+    text: string;
+    name: string;
+  }) => {
     setSearchTerm(text);
   };
+
+  const filteredStudents = GetCount?.filter((item) => {
+    const studentString = getStudentString(item).toLowerCase();
+    const searchLower = searchTerm.toLowerCase();
+    return (
+      studentString.includes(searchLower) ||
+      setStudentNum(item).toString().includes(searchLower)
+    );
+  });
+
   return (
     <Layout
       title="이전 외출 기록"
@@ -20,9 +41,28 @@ const PreviousList = () => {
         />
       }
     >
-      <div></div>
+      <ContentWrap>
+        {filteredStudents?.map((item) => (
+          <StoryList
+            id={item.id}
+            key={setStudentNum(item)}
+            name={getStudentString(item)}
+            application={item.application_cnt}
+            earlyreturn={item.early_return_cnt}
+          />
+        ))}
+      </ContentWrap>
     </Layout>
   );
 };
 
 export default PreviousList;
+
+const ContentWrap = styled.div`
+  row-gap: 12px;
+  column-gap: 60px;
+  display: flex;
+  width: fit-content;
+  justify-content: center;
+  flex-wrap: wrap;
+`;
