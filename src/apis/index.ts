7aa0d6a1aa53ca959e6c77f8049356b768b51395
hyuna <hyuna_0525@dsm.hs.kr>
2024-07-14 18:44:37 +0900

    chore : 리프레쉬토큰 설정

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,17 +1,67 @@
-import axios from "axios";
+import axios, { AxiosError, AxiosInstance, AxiosResponse } from "axios";
 import { cookie } from "@/utils/auth";
+import { useState } from "react";
 
-const BASE_URL = import.meta.env.VITE_SERVER_BASE_URL;
+const BASEURL = import.meta.env.VITE_SERVER_BASE_URL;
 
-export const instance = axios.create({
-  baseURL: BASE_URL,
+export const instance: AxiosInstance = axios.create({
+  baseURL: BASEURL,
   timeout: 10000,
 });
 
-instance.interceptors.request.use((config) => {
-  if (typeof window !== "undefined") {
-    const accessToken = cookie.get("access_token");
-    config.headers.Authorization = `Bearer ${accessToken}`;
-  }
-  return config;
+export const refreshInstance: AxiosInstance = axios.create({
+  baseURL: BASEURL,
+  timeout: 10000,
 });
+
+instance.interceptors.request.use(
+  (config) => {
+    if (typeof window !== "undefined") {
+      const accessToken = cookie.get("access_token");
+      config.headers.Authorization = `Bearer ${accessToken}`;
+    }
+    return config;
+  },
+  (error: AxiosError) => Promise.reject(error)
+);
+
+refreshInstance.interceptors.request.use(
+  (config) => {
+    const refreshToken = cookie.get("refresh_token");
+    if (refreshToken) {
+      config.headers.Authorization = `Bearer ${refreshToken}`;
+    }
+    return config;
+  },
+  (error: AxiosError) => Promise.reject(error)
+);
+
+instance.interceptors.response.use(
+  (response) => response,
+  async (error: AxiosError) => {
+    if (axios.isAxiosError(error) && error.response) {
+      const { status } = error.response;
+      if (status === 401) {
+        const refreshToken = cookie.get("refresh_token");
+        if (refreshToken) {
+          try {
+            await axios
+              .put(`${BASEURL}/admin/refresh`, null, {
+                headers: {
+                  "X-Refresh-Token": `${refreshToken}`,
+                },
+              })
+              .then((response) => {
+                const data = response.data;
+                cookie.set("access_token", data.access_token);
+                cookie.set("refresh_token", data.refresh_token);
+              });
+          } catch (refreshError) {
+            return Promise.reject(refreshError);
+          }
+        }
+      }
+    }
+    return Promise.reject(error);
+  }
+);
