11f6e7f88884d04f645e0fe4c7cd3664a1e53f49
phyuna0525 <phyunaa0525@gmail.com>
2024-07-30 23:19:35 +0900

    refactor :: 드롭다운 리팩토링

--- src/components/dropdown/index.tsx
+++ src/components/dropdown/index.tsx
@@ -1,30 +1,25 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import arrow from '@/assets/svg/dropdownImg.svg';
 import downarrow from '@/assets/svg/dropdownArrowImg2.svg';
 import * as S from './style';
 
 interface Option {
-  value: number;
   label: string;
+  value: string | number;
 }
 
-interface DropProps {
-  type: 'floor' | 'grade' | 'class' | 'club' | 'all';
-  onChange?: (selectedOption: number) => void;
+interface DropdownProp {
+  options: Option[];
+  value: number | string;
+  changeHandler: (value: string | number) => void;
 }
 
-const Dropdown: React.FC<DropProps> = ({ type, onChange }) => {
-  const [selectedGradeOption, setSelectedGradeOption] = useState<number>(1);
-  const [selectedClassOption, setSelectedClassOption] = useState<number>(5);
-  const [selectedFloorOption, setSelectedFloorOption] = useState<number>(5);
-  // const [selectedClubOption, setSelectedClubOption] =
-  //   useState<string>("세미나실 2-1(대동여지도)");
-  const [selectedAllOption, setSelectedAllOption] = useState<number>(5);
-  const [isDropdownVisible, setIsDropdownVisible] = useState<boolean>(false);
+export const Dropdown = ({ options, value, changeHandler }: DropdownProp) => {
   const dropdownRef = useRef<HTMLDivElement>(null);
+  const [isDropdownVisible, setIsDropdownVisible] = useState<boolean>(false);
 
   const toggleDropdown = () => {
-    setIsDropdownVisible(!isDropdownVisible);
+    setIsDropdownVisible((prev) => !prev);
   };
 
   useEffect(() => {
@@ -42,121 +37,31 @@ const Dropdown: React.FC<DropProps> = ({ type, onChange }) => {
     };
   }, []);
 
-  const handleOptionClick = (option: number) => {
-    if (onChange) {
-      onChange(option);
-      switch (type) {
-        case 'grade':
-          setSelectedGradeOption(option);
-          break;
-        case 'class':
-          setSelectedClassOption(option);
-          break;
-        case 'floor':
-          setSelectedFloorOption(option);
-          break;
-        // case "club":
-        //   setSelectedClubOption(option.label);
-        //   break;
-        case 'all':
-          setSelectedAllOption(option);
-          break;
-        default:
-          break;
-      }
-    }
+  const handleOptionClick = (option: Option, event: React.MouseEvent) => {
+    event.stopPropagation();
+    changeHandler(option.value);
     setIsDropdownVisible(false);
   };
 
-  const title = () => {
-    switch (type) {
-      case 'grade':
-        return `${selectedGradeOption}학년`;
-
-      case 'class':
-        return `${selectedClassOption}반`;
-
-      case 'floor': {
-        if (selectedFloorOption === 5) {
-          return `전체`;
-        } else {
-          return `${selectedFloorOption}층`;
-        }
-      }
-
-      case 'all':
-        return selectedAllOption === 5 ? `전체` : `${selectedAllOption}학년`;
-
-      // case "club":
-      //   return selectedClubOption;
-      default:
-        return '';
-    }
-  };
-
-  const options =
-    type === 'floor'
-      ? [
-          { value: 2, label: '2층' },
-          { value: 3, label: '3층' },
-          { value: 4, label: '4층' },
-          { value: 5, label: '전체' },
-        ]
-      : type === 'grade'
-        ? [
-            { value: 1, label: '1학년' },
-            { value: 2, label: '2학년' },
-            { value: 3, label: '3학년' },
-          ]
-        : type === 'class'
-          ? [
-              { value: 1, label: '1반' },
-              { value: 2, label: '2반' },
-              { value: 3, label: '3반' },
-              { value: 4, label: '4반' },
-            ]
-          : // type === "club"
-            // ? [
-            //     { value: "대동여지도", label: "세미나실 2-1(대동여지도)" },
-            //     { value: "DMS", label: "세미나실 2-2(DMS)" },
-            //     { value: "gram", label: "세미나실 2-3(gram)" },
-            //     { value: "Lift", label: "소개2실(Lift)" },
-            //     { value: "Log", label: "세미나실 3-1(Log)" },
-            //     { value: "은하", label: "세미나실 3-2(은하)" },
-            //     { value: "PiCK", label: "세미나실 3-3(PiCK)" },
-            //     { value: "어게인", label: "보안 1실(어게인)" },
-            //     { value: "info", label: "보안 2실(info)" },
-            //     { value: "TeamQSS", label: "세미나실 4-1(TeamQSS)" },
-            //     { value: "NoNamed", label: "세미나실 4-2(NoNamed)" },
-            //     { value: "Modeep", label: "세미나실 4-3(Modeep)" },
-            //     { value: "자습", label: "자습" },
-            //   ]
-            // :
-            type === 'all'
-            ? [
-                { value: 1, label: '1학년' },
-                { value: 2, label: '2학년' },
-                { value: 3, label: '3학년' },
-                { value: 5, label: '전체' },
-              ]
-            : [];
+  const selectedLabel =
+    options.find((option) => option.value === value)?.label || 'Select';
 
   return (
     <S.DropdownTitle ref={dropdownRef} onClick={toggleDropdown}>
-      {title()}
+      {selectedLabel}
       <img
-        src={isDropdownVisible ? `${downarrow}` : `${arrow}`}
+        src={isDropdownVisible ? downarrow : arrow}
         alt="arrow"
         width={16}
         height={16}
       />
       {isDropdownVisible && (
-        <S.DropdownClickContainer isActive>
+        <S.DropdownClickContainer isActive={true}>
           {options.map((option) => (
             <S.DropdownClick
-              isActive
-              key={option.value.toString()}
-              onClick={() => handleOptionClick(option.value)}
+              key={option.value}
+              onClick={(event) => handleOptionClick(option, event)}
+              isActive={false}
             >
               {option.label}
             </S.DropdownClick>
@@ -166,5 +71,3 @@ const Dropdown: React.FC<DropProps> = ({ type, onChange }) => {
     </S.DropdownTitle>
   );
 };
-
-export default Dropdown;
