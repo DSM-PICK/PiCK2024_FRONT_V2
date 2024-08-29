525b83e2235c49f17a31385e766305fd373ffa59
phyuna0525 <phyunaa0525@gmail.com>
2024-08-29 20:02:43 +0900

    add :: 반별 조회 api연동

--- src/apis/weekend-meals/index.ts
+++ src/apis/weekend-meals/index.ts
@@ -27,3 +27,19 @@ export const ChangeState = () => {
     },
   });
 };
+
+export const GetClassWeekendMeal = (grade: number, class_num: number) => {
+  return useQuery({
+    queryKey: ['GetClassWeekendMeal', grade, class_num],
+    queryFn: async () => {
+      try {
+        const { data } = await instance.get<GetAllMealsType[]>(
+          `${router}/all?grade=${grade}&class_num=${class_num}`,
+        );
+        return data;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  });
+};
