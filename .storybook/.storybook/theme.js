054b5d72e907a5e717dd813f7544696c6b08ecfd
hyuna <hyuna_0525@dsm.hs.kr>
2024-09-03 10:59:42 +0900

    chore :: 스토리북 커스텀 및 docs설정

---
+++ .storybook/.storybook/theme.js
@@ -0,0 +1,7 @@
+import { create } from '@storybook/theming';
+import PICKLOGO from './public/images/PiCK.png';
+
+export default create({
+  brandTitle: 'PiCK Design System',
+  brandImage: PICKLOGO,
+});
