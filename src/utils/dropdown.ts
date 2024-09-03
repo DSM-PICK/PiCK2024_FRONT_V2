fc6591922bbf0ef02b522e10252baeb41b4fa3f2
phyuna0525 <phyunaa0525@gmail.com>
2024-09-03 11:32:00 +0900

    chore :: 드롭다운 배열 수정

--- src/utils/dropdown.ts
+++ src/utils/dropdown.ts
@@ -1,15 +1,15 @@
 export const GradeOption = [
-  { label: '전체', value: 5 },
   { label: '1학년', value: 1 },
   { label: '2학년', value: 2 },
   { label: '3학년', value: 3 },
+  { label: '전체', value: 5 },
 ];
 export const Class_numOption = [
-  { label: '전체', value: 5 },
   { label: '1반', value: 1 },
   { label: '2반', value: 2 },
   { label: '3반', value: 3 },
   { label: '4반', value: 4 },
+  { label: '전체', value: 5 },
 ];
 export const FloorOption = [
   { label: '2층', value: 2 },
