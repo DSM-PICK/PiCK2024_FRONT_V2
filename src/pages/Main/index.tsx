6ada686b5e7ee27886227c05127d42fc288f5a07
hyuna <phyunaa0525@gmail.com>
2024-11-12 13:38:14 +0900

    feat :: 자동완성 인풋 수정

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -2,10 +2,10 @@ import Header from '@/components/header';
 import * as S from './style';
 import mainLogo from '@/assets/svg/mainLogo.svg';
 import MainRouterButton from '@/components/mainRouterButton';
-import { showToast } from '@/components/toast';
-import { useEffect } from 'react';
+import { useGetAllTeacher } from '@/apis/admin';
 
 const Main = () => {
+  useGetAllTeacher();
   return (
     <>
       <Header />
