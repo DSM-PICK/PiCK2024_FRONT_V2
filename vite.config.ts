0ceb244ee0c7af98065b96ac81ad64afcbe71b61
HYUNA <phyunaa0525@gmail.com>
2025-03-05 10:25:31 +0900

    CHORE :: vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,10 @@ export default defineConfig({
   server: {
     host: "0.0.0.0",
     port: 3000,
+    allowedHosts: [
+      "pick-admin-stag.xquare.app",
+      "pick-admin.xquare.app",
+    ],
   },
   resolve: {
     alias: {
