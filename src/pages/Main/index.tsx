8eeeddd6888a22587d5dbe9c613832d959e63b60
kimsiwoo <ksw98035213@gmail.com>
2025-12-02 11:55:46 +0900

    fix ::코드레빗 반영

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -5,10 +5,13 @@ import MainRouterButton from '@/components/mainRouterButton';
 import { useGetAllTeacher } from '@/apis/admin';
 
 const Main = () => {
-  const userAgent = navigator.userAgent;
-  if (/iPad|iPhone|iPod/.test(userAgent) || /android/i.test(userAgent)) {
-    alert('알맞지 않은 기기입니다. pick-teacher 로 이동합니다');
-    location.href = 'https://pick-teacher.dsmhs.kr';
+  if (navigator !== undefined) {
+    const userAgent = navigator.userAgent;
+    if (/iPad|iPhone|iPod/.test(userAgent) || /android/i.test(userAgent)) {
+      alert('알맞지 않은 기기입니다. pick-teacher 로 이동합니다');
+      location.href = 'https://pick-teacher.dsmhs.kr';
+      return null;
+    }
   }
   useGetAllTeacher();
   return (
